26ed58d3b016ae8a192958c200b636ad46ab03a4
mr8god <17696748602@163.com>
2020-07-29 23:52:16 +0800

    Initial commit

---
+++ src/cn/mr8god/refactor_first/Movie.java
@@ -0,0 +1,20 @@
+package cn.mr8god.refactor_first;
+
+/**
+ * @author Mr8god
+ * @date 20200729
+ * @time 23:46:07
+ */
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String title;
+    private int priceCode;
+
+    public Movie(String title, int priceCode) {
+
+    }
+
+}
